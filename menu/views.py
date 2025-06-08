5db96aa96c06652b91ac6a8209fd5c48fc2c6f57
parhammohtasham <206984336+parhammohtasham@users.noreply.github.com>
2025-06-08 05:45:07 -0700

    pages
    pages application done

--- menu/views.py
+++ menu/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Category
 # Create your views here.
 
-#def category_view(request):
-   # categories = Category.objects.all()  # دریافت همه دسته‌بندی‌ها
-    #return render(request, 'menu/categories.html', {'categories': categories})
+def category_view(request):
+    categories = Category.objects.all()   #دریافت همه دسته‌بندی‌ها
+    return render(request, 'menu/categories.html', {'categories': categories})
+
